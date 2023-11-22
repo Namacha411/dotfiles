63739d3d07ae97f5779be2aa9fb670b4f4b09417
tanaka hideyuki <thdyk.4.11@gmail.com>
2023-11-22 22:58:23 +0900

    update scoop repo

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,18 +2,20 @@ local wezterm = require("wezterm")
 
 return {
 	window_background_opacity = 0.95,
-  font = wezterm.font({ family = "CaskaydiaCove Nerd Font" }),
-  font_size = 13.0,
-  line_height = 1.2,
+	font = wezterm.font({ family = "CaskaydiaCove Nerd Font" }),
+	font_size = 13.0,
+	line_height = 1.2,
 	window_padding = {
 		left = 20,
 		right = 20,
 		top = 20,
 		bottom = 20,
 	},
-  max_fps = 240,
+	max_fps = 240,
 	default_prog = { "pwsh.exe" },
-	color_scheme = "OneHalfDark",
+	--color_scheme = "OneHalfDark",
+	-- color_scheme = "iceberg-dark",
+	color_scheme = "One Dark (Gogh)",
 	enable_tab_bar = false,
 	enable_scroll_bar = false,
 }
