1856f1ad37cb01790214e54a572ce2c7af393c85
tanaka hideyuki <thdyk.4.11@gmail.com>
2023-11-22 22:45:06 +0900

    update scoop

--- setup.ps1
+++ setup.ps1
@@ -4,9 +4,19 @@ function ln($src, $dst) {
   New-Item -ItemType SymbolicLink -Path $dst -Value $value
 }
 
+function setupscoop() {
+  if ( -not ( Test-Path scoop ) ) {
+    irm get.scoop.sh | iex
+  }
+  scoop install ./scoop/scoopfile.json
+}
+
 function makesymlink() {
   ln ./powershell/profile.ps1 $profile
   ln ./wezterm/.wezterm.lua $home/.wezterm.lua
   ln ./nvim $env:LOCALAPPDATA/nvim
   ln ./starship/starship.toml $home/.config/starship.toml
 }
+
+setupscoop()
+makesymlink()
