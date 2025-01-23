8463b26128cb05f63a54bf7ebd7d18c5cab11860
tanaka hideyuki <thdyk.4.11@gmail.com>
2025-01-23 18:15:55 +0900

    [modify] profile.ps1

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,102 +1,70 @@
-# PATH
-$Env:GOPATH = "$HOME\go"
-$Env:PATH = "C:`\Users`\thdyk`\.local`\bin;$Env:PATH"
-
 # Prompt
+Invoke-Expression (& { (starship init powershell --print-full-init | Out-String) })
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
+
 $OutputEncoding = [System.Text.Encoding]::GetEncoding('utf-8')
-Invoke-Expression (&starship init powershell)
 
 # PSReadLine
 Import-Module -Name CompletionPredictor
 
-function _OnViModeChange() {
+function __OnViModeChange {
     if ($args[0] -eq 'Command') {
         # Set the cursor to a blinking block.
-        Write-Host -NoNewLine "$([char]0x1b)[1 q"
+        Write-Host -NoNewLine "`e[1 q"
     } else {
         # Set the cursor to a blinking line.
-        Write-Host -NoNewLine "$([char]0x1b)[5 q"
+        Write-Host -NoNewLine "`e[5 q"
     }
-}
-$AddToHistoryDelegate = {
-    param([string]$line)
-    switch -regex ($line) {
-      "^[0-9]" { return $false }
-      "^[a-z]$" { return $false }
-      "exit" { return $false }
-    }
-    $sensitive = "password|asplaintext|token|key|secret|credential"
-    return ($line -notmatch $sensitive)
 }
 Set-PSReadLineOption -EditMode Vi
-Set-PSReadLineOption -AddToHistoryHandler $AddToHistoryDelegate
-Set-PSReadlineOption -HistoryNoDuplicates
+Set-PSReadLineOption -ViModeIndicator Script -ViModeChangeHandler $Function:__OnViModeChange
+# https://github.com/PowerShell/PSReadLine/issues/906
+Set-PSReadLineKeyHandler -Chord 'Ctrl+Oem4' -ViMode Insert -Function ViCommandMode
+
+Set-PSReadLineOption -HistorySavePath "$HOME\$($Host.Name)_history.txt"
 Set-PSReadlineOption -HistorySearchCursorMovesToEnd
 Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+
 Set-PSReadLineOption -PredictionViewStyle ListView
-Set-PSReadLineOption -ViModeIndicator Script -ViModeChangeHandler $Function:_OnViModeChange
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
-Set-PSReadLineKeyHandler -Key "Ctrl+f" -Function ForwardWord
+
+Set-PSReadLineOption -BellStyle Visual
 
 # Alias
-function _ls() {
-  eza -a --icons
+Remove-Item alias:cd
+Remove-Item alias:ls
+Remove-Item alias:cat
+Remove-Item alias:ps
+
+function cd($Path="$HOME") {
+  z $Path && eza --icons
 }
-function ll() {
-  eza -a --tree --icons --long
+function ls {
+  eza --icons
 }
-function touch() {
+function ll {
+  eza -a --icons --long
+}
+function touch {
   New-Item $args -Type File 
 }
-function cl($path) {
-  Set-Location $path && eza --icons
+function mkcd($Path) {
+  New-Item $Path -ItemType Directory && z $Path
 }
-function mkcd($dirName) {
-  New-Item $dirName -ItemType Directory && Set-Location $dirName
-}
-function ..() {
-  Set-Location ..
-}
-function ...() {
-  Set-Location ../..
-}
-function ~() {
-  Set-Location ~
+function rmrf {
+  Remove-Item -Recurse -Force $args
 }
 function :q() {
   Exit
 }
-function :wq() {
-  Exit
-}
 
 Set-Alias sudo gsudo
-Set-Alias ls _ls
 Set-Alias cat bat
 Set-Alias find fd
 Set-Alias od hexyl
 Set-Alias ps procs
-Set-Alias grep rg
 Set-Alias unzip Expand-Archive
 
-# Fzf setting
-function ff() {
-  return (fzf --preview "bat --color=always --style=header,grid --line-range :100 {}")
-}
-
-# func
-function ktc($kt) {
-  return (kotlinc $kt -include-runtime -d a.jar)
-}
-
-function ktrun($kt) {
-  Write-Host "Compiling..."
-  kotlinc $kt -include-runtime -d a.jar 
-  if ($?) {
-    Write-Host "Compiled!"
-    java -jar a.jar
-  }
-  if ($?) {
-    Remove-Item a.jar
-  }
-}
+# PATH
+$Env:GOPATH = "$HOME\go"
+$Env:PATH = "$HOME\.local\bin;$Env:PATH"
